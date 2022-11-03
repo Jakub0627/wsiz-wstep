a55718612882c7892570f9a4892e55cc3c1fbdc4
Jakub Gładysz <268880@student.pwr.edu.pl>
2022-11-03 17:26:36 +0100

    Poprawki repozytorium...

---
+++ Lab3/zad3.py
@@ -0,0 +1,11 @@
+print("pętla będzie się wykonywać do momentu podania liczby ujemnej: ")
+a = 0
+while (a>=0):
+    a = int(input("Podaj liczbę"))
+print("pętla została przerwana")
+
+'''
+while True:
+    x = int(input("napisz liczbę całkowitą:"))
+    if x<0:
+    break'''
